27fbd42b7e2627f61d3ad436cad9bcaf0e393199
莲子 <15502723800@163.com>
2016-12-04 14:59:56 +0800

    view  frame Extention

--- WMTools/Classes/UIView+Extention.h
+++ WMTools/Classes/UIView+Extention.h
@@ -10,4 +10,39 @@
 
 @interface UIView (Extention)
 
+
+
+/**获得view的X坐标*/
+-(CGFloat)x;
+/**获得view的Y坐标*/
+-(CGFloat)y;
+/**view的中心X坐标*/
+-(CGFloat)centerX;
+/**获得view的中心Y坐标*/
+-(CGFloat)centerY;
+/**获得view的宽度*/
+-(CGFloat)width;
+/**获得view的高度*/
+-(CGFloat)height;
+/**获得view的大小*/
+-(CGSize)size;
+/**获得view的左上角坐标*/
+-(CGPoint)origin;
+
+/**设置view的X坐标*/
+-(void)setX:(CGFloat)x;
+/**设置view的Y坐标*/
+-(void)setY:(CGFloat)y;
+/**设置view的中心X坐标*/
+-(void)setCenterX:(CGFloat)centerX;
+/**设置view的中心Y坐标*/
+-(void)setCenterY:(CGFloat)centerY;
+/**设置view的宽度*/
+-(void)setWidth:(CGFloat)width;
+/**设置view的高度*/
+-(void)setHeight:(CGFloat)height;
+/**设置view的大小*/
+-(void)setSize:(CGSize)size;
+/**设置view的左上角坐标*/
+-(void)setOrigin:(CGPoint)origin;
 @end
