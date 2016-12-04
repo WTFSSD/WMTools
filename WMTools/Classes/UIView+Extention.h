57b80d0da2b68980dc5359eaef1c117d0bf5fba0
莲子 <15502723800@163.com>
2016-12-04 14:12:11 +0800

    233

---
+++ WMTools/Classes/UIView+Extention.h
@@ -0,0 +1,13 @@
+//
+//  UIView+Extention.h
+//  WMTools
+//
+//  Created by WTFSSD on 2016/12/4.
+//  Copyright © 2016年 WTFSSD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Extention)
+
+@end
---
+++ WMTools/Classes/UIView+Extention.m
@@ -0,0 +1,13 @@
+//
+//  UIView+Extention.m
+//  WMTools
+//
+//  Created by WTFSSD on 2016/12/4.
+//  Copyright © 2016年 WTFSSD. All rights reserved.
+//
+
+#import "UIView+Extention.h"
+
+@implementation UIView (Extention)
+
+@end
